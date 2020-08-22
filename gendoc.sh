6c79471c6716c3876ca7282b4a340ed90f703de4
ismdeep <ismdeep@protonmail.com>
2020-08-22 23:00:12 +0800

    Add gendoc.sh

---
+++ gendoc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+current_path=`pwd`
+
+categories=(
+    "01-text-processing"
+    "02-network"
+    "03-class"
+    "04-thread-processing"
+    "05-web-application"
+    "06-file"
+    "07-database"
+    "08-multimedia"
+    "09-games")
+
+echo "# Programming Project List
+
+" > task.md
+
+for category in ${categories[@]};do
+    cat ${category}/README.md >> task.md
+done
